b87bff80e1050cf7d413970f5111067cc2c192ae
denchu <kuroko4491@yahoo.co.jp>
2018-01-06 13:48:50 +0900

    キャラクターの基底クラスを実装 Playerの移動(左右に動くだけ)を実装
    aaaaaaa

---
+++ Assets/Denchu/Script/BaseCharacterController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacterController : MonoBehaviour {
+
+    protected float hp = 10.0f;
+    protected float hpMax = 10.0f;
+    protected float dir = 1.0f;
+    protected float speed = 1.0f;
+    protected float basScaleX = 1.0f;
+    protected float speedVx = 0.0f;
+    protected Rigidbody2D rb;
+
+    protected virtual void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        dir = (transform.localScale.x > 0.0f) ? 1 : -1;
+        basScaleX = transform.localScale.x * dir;
+        transform.localScale = new Vector3(basScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+	// Use this for initialization
+	protected virtual void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+		
+	}
+
+    protected virtual void FixedUpdate() {
+        FixedUpdateCharacter();
+
+        rb.velocity = new Vector2(speedVx, rb.velocity.y);
+        Debug.Log(rb.velocity);
+    }
+
+    protected virtual void FixedUpdateCharacter() {
+        
+    }
+
+    public virtual void ActionMove(float n)
+    {
+        if (n != 0.0f){
+            dir = Mathf.Sign(n);
+            speedVx = speed * n;
+        } else {
+            speedVx = 0;
+        }
+    }
+
+    public virtual bool SetHp(float _hp, float _hpMax) {
+        hp = _hp;
+        hpMax = _hpMax;
+        return (hp <= 0);
+    }
+
+}
---
+++ Assets/Denchu/Script/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BaseCharacterController {
+
+    public float initHpMax = 20.0f;
+    public float initSpeed = 12.0f;
+
+    protected override void Awake() {
+        base.Awake();
+
+        // パラメータ初期化
+        speed = initSpeed;
+        SetHp(initHpMax, initHpMax);
+    }
+
+    protected override void Update() {
+        // 矢印の左右の入力検出
+        //float joyMv = Input.GetAxis("Horizontal");
+        float vx = 0.0f;
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            vx -= 1.0f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            vx += 1.0f;
+        }
+        ActionMove(vx);
+    }
+
+    protected override void FixedUpdateCharacter() {
+        transform.localScale = new Vector3(basScaleX * dir, transform.localScale.y, transform.localScale.z);
+
+        // 試験的
+        
+    }
+
+    public override void ActionMove(float n) {
+        base.ActionMove(n);
+    }
+}
